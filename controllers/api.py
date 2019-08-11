ff782645242dc06255d6fa5e64f7a5386b363b95
Alex Pedersen <alex@getofficeapp.com>
2019-08-11 13:06:50 +0200

    ADD: Rest of base calls + OpenAPI config
    TODO: Add parameters in openAPI config.

--- controllers/api.py
+++ controllers/api.py
@@ -14,29 +14,45 @@ def get_city(name: str) -> JSON:
         target=(
             lambda: city.where('lower(Name) = ?', [name.lower()]).to_json()
         )
-    )
+    ).start()
 
 
 def get_city_all():
     return Thread(target=(lambda: city.all().to_json())).start()
 
 
+def get_country(name: str) -> JSON:
+    return Thread(
+        target=(
+            lambda: country.where('lower(Name) = ?',  [name.lower()]).to_json()
+        )
+    ).start()
+
+
+def get_country_all() -> JSON:
+    return Thread(target=lambda: city.all().to_json()).start()
+
+
 # Country info calls
-def get_countryinfo(name: str):
-    return NotImplementedError
-
-
-def get_countryinfo_all():
-    return NotImplementedError
+def get_countryinfo(id: str):
+    return Thread(
+        target=(
+            lambda: countryinfo.where('_id = ?',  [id.lower()]).to_json()
+        )
+    ).start()
 
 
 # Country language calls
-def get_countrylanguage(name: str):
-    return NotImplementedError
+def get_countrylanguage(language: str):
+    return Thread(
+        target=(
+            lambda: countrylanguage.where(
+                'lower(Language) = ?',
+                [language.lower()]
+            ).to_json()
+        )
+    ).start()
 
 
 def get_countrylanguage_all():
-    return NotImplementedError
-
-
-
+    return Thread(target=lambda: countrylanguage.all().to_json()).start()
