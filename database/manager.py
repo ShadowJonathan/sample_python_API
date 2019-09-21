d8b9ade3a67a2a0cf49f96c065e37b4c9718d713
Alex Pedersen <alex@getofficeapp.com>
2019-09-22 00:57:33 +0200

    UPDATE: Database Manager is now polymorphic and uses .env variables

--- database/manager.py
+++ database/manager.py
@@ -1,15 +1,55 @@
-from orator import DatabaseManager, Model
+# Python core imports
+import os
 
-config = {
-    'mysql': {
-        'driver': 'mysql',
-        'host': 'Localhost',
-        'database': 'world_x',
-        'user': 'root',
-        'password': '',
-        'prefix': ''
-    }
-}
+# Third-party imports
+from orator import DatabaseManager
+from orator import Model
 
-db = DatabaseManager(config)
-Model.set_connection_resolver(db)
+# Importing local models
+from models.city import city
+from models.country import country
+from models.countryinfo import countryinfo
+from models.countrylanguage import countrylanguage
+
+
+class Database:
+
+    db = None
+    config = {}
+
+    City = None
+    Country = None
+    CountryInfo = None
+    CountryLanguage = None
+
+    # Config
+    config = {}
+
+    def __init__(self):
+        # Bind Models to local variables
+        self.City = city
+        self.Country = country
+        self.CountryInfo = countryinfo
+        self.CountryLanguage = countrylanguage
+
+        # Set config
+        self.config = {
+            'mysql': {
+                'driver': 'mysql',
+                'host': os.getenv('DB_HOST'),
+                'database': os.getenv('DB_NAME'),
+                'user': os.getenv('DB_USER'),
+                'password': os.getenv('DB_PASSWORD'),
+                'prefix': ''
+            }
+        }
+
+        # Create database from config
+        self.db = DatabaseManager(self.config)
+
+        # Auto-resolve connection
+        Model.set_connection_resolver(self.db)
+
+
+# Create public instance
+db = Database()
