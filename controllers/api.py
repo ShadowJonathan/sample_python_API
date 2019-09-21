f595a4119929c885c6ca11c168f256a23d177e34
Alex Pedersen <alex@getofficeapp.com>
2019-09-22 00:56:31 +0200

    UPDATE: API Controller now follows basic "KISS" principle

--- controllers/api.py
+++ controllers/api.py
@@ -1,59 +1,81 @@
-# Python core imports
-from threading import Thread
-import json as JSON
-
-# Local model imports
-from models.city import city
-from models.country import country
-from models.countryinfo import countryinfo
-from models.countrylanguage import countrylanguage
+# Local imports
+from database.manager import db
 
 
 # City calls
-def get_city(name: str) -> JSON:
-    return Thread(
-        target=(
-            lambda: city.where('lower(Name) = ?', [name.lower()]).to_json()
-        )
-    ).start()
+def get_city(name: str) -> dict:
+    """
+    API Controlled method to retrieve city details for specified city
+    Arguements:
+    - name <str> name of the city
+
+    Returns a dict with information about the requested city
+    """
+    return db.City.where('Name', '=', name.capitalize()).get().serialize()
 
 
 def get_city_all():
-    return Thread(target=(lambda: city.all().to_json())).start()
+    """
+    API Controlled method to retrieve city details for all available cities
+
+    Returns a dict with information about all available cities
+    """
+    return db.City.all().serialize()
 
 
-def get_country(name: str) -> JSON:
-    return Thread(
-        target=(
-            lambda: country.where('lower(Name) = ?',  [name.lower()]).to_json()
-        )
-    ).start()
+def get_country(name: str) -> dict:
+    """
+    API Controlled method to retrieve country details for specified country
+    Arguements:
+    - name <str> name of the country
+
+    Returns a dict with information about the requested city
+    """
+    return db.Country.where('Name', '=',  name.capitalize()).get().serialize()
 
 
-def get_country_all() -> JSON:
-    return Thread(target=lambda: city.all().to_json()).start()
+def get_country_all() -> dict:
+    """
+    API Controlled method to retrieve country details for
+    all available countries
+
+    Returns a dict with information about all available countries
+    """
+    return db.Country.all().serialize()
 
 
 # Country info calls
 def get_countryinfo(id: str):
-    return Thread(
-        target=(
-            lambda: countryinfo.where('_id = ?',  [id.lower()]).to_json()
-        )
-    ).start()
+    """
+    API Controlled method to retrieve:
+    Arguements:
+    - id <str> ID code of a specific country
+
+    Returns a dict with detailed information about the requested country
+    """
+    return db.CountryInfo.where('_id', '=', id.upper()).get().serialize()
 
 
 # Country language calls
 def get_countrylanguage(language: str):
-    return Thread(
-        target=(
-            lambda: countrylanguage.where(
-                'lower(Language) = ?',
-                [language.lower()]
-            ).to_json()
-        )
-    ).start()
+    """
+    API Controlled method to retrieve:
+    Arguements:
+    - language <str> language of the country to retrieve
+
+    Returns a dict with requested data
+    """
+    return db.CountryLanguage.where('Language', '=', language.capitalize()) \
+             .get() \
+             .serialize()
 
 
 def get_countrylanguage_all():
-    return Thread(target=lambda: countrylanguage.all().to_json()).start()
+    """
+    API Controlled method to retrieve:
+    Arguements:
+    -
+
+    Returns a dict with requested data
+    """
+    return db.CountryLanguage.all().get().serialize()
